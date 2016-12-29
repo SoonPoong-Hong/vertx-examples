5330d7ef4203e7424e1d4af68aa1b15b970006dd
rocklike <rocklike@gmail.com>
2016-12-30 01:31:51 +0900

    .

--- core-examples/src/main/java/io/vertx/example/core/http/proxy/Proxy.java
+++ core-examples/src/main/java/io/vertx/example/core/http/proxy/Proxy.java
@@ -1,6 +1,7 @@
 package io.vertx.example.core.http.proxy;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpClientRequest;
@@ -11,34 +12,39 @@ import io.vertx.example.util.Runner;
  */
 public class Proxy extends AbstractVerticle {
 
-  // Convenience method so you can run it in your IDE
-  public static void main(String[] args) {
-    Runner.runExample(Proxy.class);
-  }
+	// Convenience method so you can run it in your IDE
+	public static void main(String[] args) {
+		Runner.runExample(Proxy.class);
+	}
 
-  @Override
-  public void start() throws Exception {
-    HttpClient client = vertx.createHttpClient(new HttpClientOptions());
-    vertx.createHttpServer().requestHandler(req -> {
-      System.out.println("Proxying request: " + req.uri());
-      HttpClientRequest c_req = client.request(req.method(), 8282, "localhost", req.uri(), c_res -> {
-        System.out.println("Proxying response: " + c_res.statusCode());
-        req.response().setChunked(true);
-        req.response().setStatusCode(c_res.statusCode());
-        req.response().headers().setAll(c_res.headers());
-        c_res.handler(data -> {
-          System.out.println("Proxying response body: " + data.toString("ISO-8859-1"));
-          req.response().write(data);
-        });
-        c_res.endHandler((v) -> req.response().end());
-      });
-      c_req.setChunked(true);
-      c_req.headers().setAll(req.headers());
-      req.handler(data -> {
-        System.out.println("Proxying request body " + data.toString("ISO-8859-1"));
-        c_req.write(data);
-      });
-      req.endHandler((v) -> c_req.end());
-    }).listen(8080);
-  }
+	@Override
+	public void start() throws Exception {
+		HttpClient client = vertx.createHttpClient(new HttpClientOptions());
+		vertx.createHttpServer().requestHandler(req -> {
+			System.out.println("===== Proxying request: " + req.remoteAddress() + "::" +  req.uri());
+			HttpClientRequest c_req = client.request(req.method(), 8080, "localhost", req.uri(), c_res -> {
+				System.out.println("== Proxying response: " + c_res.statusCode());
+				MultiMap headers = c_res.headers();
+				System.out.println("== Proxying response headers: ");
+				headers.forEach(e-> System.out.println("\t" + e));
+				System.out.println("== header 끝.");
+
+				req.response().setChunked(true);
+				req.response().setStatusCode(c_res.statusCode());
+				req.response().headers().setAll(c_res.headers());
+				c_res.handler(data -> {
+					System.out.println("== Proxying response body: " + data.length() + " => " + data.toString("ISO-8859-1"));
+					req.response().write(data);
+				});
+				c_res.endHandler((v) -> req.response().end());
+			});
+			c_req.setChunked(true);
+			c_req.headers().setAll(req.headers());
+			req.handler(data -> {
+				System.out.println("== Proxying request body " + data.toString("ISO-8859-1"));
+				c_req.write(data);
+			});
+			req.endHandler((v) -> c_req.end());
+		}).listen(80);
+	}
 }
