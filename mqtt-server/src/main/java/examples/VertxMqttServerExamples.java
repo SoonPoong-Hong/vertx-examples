2fc8f339f0423fface51997598db3b1a89014047
rocklike <rocklike@gmail.com>
2017-03-06 00:40:12 +0900

    .

--- mqtt-server/src/main/java/examples/VertxMqttServerExamples.java
+++ mqtt-server/src/main/java/examples/VertxMqttServerExamples.java
@@ -34,244 +34,254 @@ import java.util.List;
 //@Source
 public class VertxMqttServerExamples {
 
-  /**
-   * Example for handling client connection
-   * @param vertx
-   */
-  public void example1(Vertx vertx) {
+	/**
+	 * Example for handling client connection
+	 *
+	 * @param vertx
+	 */
+	public void example1(Vertx vertx) {
 
-    MqttServer mqttServer = MqttServer.create(vertx);
-    mqttServer.endpointHandler(endpoint -> {
+		MqttServer mqttServer = MqttServer.create(vertx);
+		mqttServer.endpointHandler(endpoint -> {
 
-      // shows main connect info
-      System.out.println("MQTT client [" + endpoint.clientIdentifier() + "] request to connect, clean session = " + endpoint.isCleanSession());
+			// shows main connect info
+			System.out.println("MQTT client [" + endpoint.clientIdentifier() + "] request to connect, clean session = "
+					+ endpoint.isCleanSession());
 
-      if (endpoint.auth() != null) {
-        System.out.println("[username = " + endpoint.auth().userName() + ", password = " + endpoint.auth().password() + "]");
-      }
-      if (endpoint.will() != null) {
-        System.out.println("[will topic = " + endpoint.will().willTopic() + " msg = " + endpoint.will().willMessage() +
-          " QoS = " + endpoint.will().willQos() + " isRetain = " + endpoint.will().isWillRetain() + "]");
-      }
+			if (endpoint.auth() != null) {
+				System.out.println("[username = " + endpoint.auth().userName() + ", password = "
+						+ endpoint.auth().password() + "]");
+			}
+			if (endpoint.will() != null) {
+				System.out.println("[will topic = " + endpoint.will().willTopic() + " msg = "
+						+ endpoint.will().willMessage() + " QoS = " + endpoint.will().willQos() + " isRetain = "
+						+ endpoint.will().isWillRetain() + "]");
+			}
 
-      System.out.println("[keep alive timeout = " + endpoint.keepAliveTimeSeconds() + "]");
+			System.out.println("[keep alive timeout = " + endpoint.keepAliveTimeSeconds() + "]");
 
-      // accept connection from the remote client
-      endpoint.accept(false);
+			// accept connection from the remote client
+			endpoint.accept(false);
 
-    })
-      .listen(ar -> {
+		}).listen(ar -> {
 
-        if (ar.succeeded()) {
+			if (ar.succeeded()) {
 
-          System.out.println("MQTT server is listening on port " + ar.result().actualPort());
-        } else {
+				System.out.println("MQTT server is listening on port " + ar.result().actualPort());
+			} else {
 
-          System.out.println("Error on starting the server");
-          ar.cause().printStackTrace();
-        }
-      });
-  }
+				System.out.println("Error on starting the server");
+				ar.cause().printStackTrace();
+			}
+		});
+	}
 
-  /**
-   * Example for handling client disconnection
-   * @param endpoint
-   */
-  public void example2(MqttEndpoint endpoint) {
+	/**
+	 * Example for handling client disconnection
+	 *
+	 * @param endpoint
+	 */
+	public void example2(MqttEndpoint endpoint) {
 
-    // handling disconnect message
-    endpoint.disconnectHandler(v -> {
+		// handling disconnect message
+		endpoint.disconnectHandler(v -> {
 
-      System.out.println("Received disconnect from client");
-    });
-  }
+			System.out.println("Received disconnect from client");
+		});
+	}
 
-  /**
-   * Example for handling client connection using SSL/TLS
-   * @param vertx
-   */
-  public void example3(Vertx vertx) {
+	/**
+	 * Example for handling client connection using SSL/TLS
+	 *
+	 * @param vertx
+	 */
+	public void example3(Vertx vertx) {
 
-    MqttServerOptions options = new MqttServerOptions()
-      .setPort(8883)
-      .setKeyCertOptions(new PemKeyCertOptions()
-        .setKeyPath("./src/test/resources/tls/server-key.pem")
-        .setCertPath("./src/test/resources/tls/server-cert.pem"))
-      .setSsl(true);
+		MqttServerOptions options = new MqttServerOptions().setPort(8883)
+				.setKeyCertOptions(new PemKeyCertOptions().setKeyPath("./src/test/resources/tls/server-key.pem")
+						.setCertPath("./src/test/resources/tls/server-cert.pem"))
+				.setSsl(true);
 
-    MqttServer mqttServer = MqttServer.create(vertx, options);
-    mqttServer.endpointHandler(endpoint -> {
+		MqttServer mqttServer = MqttServer.create(vertx, options);
+		mqttServer.endpointHandler(endpoint -> {
 
-      // shows main connect info
-      System.out.println("MQTT client [" + endpoint.clientIdentifier() + "] request to connect, clean session = " + endpoint.isCleanSession());
+			// shows main connect info
+			System.out.println("MQTT client [" + endpoint.clientIdentifier() + "] request to connect, clean session = "
+					+ endpoint.isCleanSession());
 
-      if (endpoint.auth() != null) {
-        System.out.println("[username = " + endpoint.auth().userName() + ", password = " + endpoint.auth().password() + "]");
-      }
-      if (endpoint.will() != null) {
-        System.out.println("[will topic = " + endpoint.will().willTopic() + " msg = " + endpoint.will().willMessage() +
-          " QoS = " + endpoint.will().willQos() + " isRetain = " + endpoint.will().isWillRetain() + "]");
-      }
+			if (endpoint.auth() != null) {
+				System.out.println("[username = " + endpoint.auth().userName() + ", password = "
+						+ endpoint.auth().password() + "]");
+			}
+			if (endpoint.will() != null) {
+				System.out.println("[will topic = " + endpoint.will().willTopic() + " msg = "
+						+ endpoint.will().willMessage() + " QoS = " + endpoint.will().willQos() + " isRetain = "
+						+ endpoint.will().isWillRetain() + "]");
+			}
 
-      System.out.println("[keep alive timeout = " + endpoint.keepAliveTimeSeconds() + "]");
+			System.out.println("[keep alive timeout = " + endpoint.keepAliveTimeSeconds() + "]");
 
-      // accept connection from the remote client
-      endpoint.accept(false);
+			// accept connection from the remote client
+			endpoint.accept(false);
 
-    })
-      .listen(ar -> {
+		}).listen(ar -> {
 
-        if (ar.succeeded()) {
+			if (ar.succeeded()) {
 
-          System.out.println("MQTT server is listening on port " + ar.result().actualPort());
-        } else {
+				System.out.println("MQTT server is listening on port " + ar.result().actualPort());
+			} else {
 
-          System.out.println("Error on starting the server");
-          ar.cause().printStackTrace();
-        }
-      });
-  }
+				System.out.println("Error on starting the server");
+				ar.cause().printStackTrace();
+			}
+		});
+	}
 
-  /**
-   * Example for handling client subscription request
-   * @param endpoint
-   */
-  public void example4(MqttEndpoint endpoint) {
+	/**
+	 * Example for handling client subscription request
+	 *
+	 * @param endpoint
+	 */
+	public void example4(MqttEndpoint endpoint) {
 
-    // handling requests for subscriptions
-    endpoint.subscribeHandler(subscribe -> {
+		// handling requests for subscriptions
+		endpoint.subscribeHandler(subscribe -> {
 
-      List<MqttQoS> grantedQosLevels = new ArrayList<>();
-      for (MqttTopicSubscription s: subscribe.topicSubscriptions()) {
-        System.out.println("Subscription for " + s.topicName() + " with QoS " + s.qualityOfService());
-        grantedQosLevels.add(s.qualityOfService());
-      }
-      // ack the subscriptions request
-      endpoint.subscribeAcknowledge(subscribe.messageId(), grantedQosLevels);
+			List<MqttQoS> grantedQosLevels = new ArrayList<>();
+			for (MqttTopicSubscription s : subscribe.topicSubscriptions()) {
+				System.out.println("Subscription for " + s.topicName() + " with QoS " + s.qualityOfService());
+				grantedQosLevels.add(s.qualityOfService());
+			}
+			// ack the subscriptions request
+			endpoint.subscribeAcknowledge(subscribe.messageId(), grantedQosLevels);
 
-    });
-  }
+		});
+	}
 
-  /**
-   * Example for handling client unsubscription request
-   * @param endpoint
-   */
-  public void example5(MqttEndpoint endpoint) {
+	/**
+	 * Example for handling client unsubscription request
+	 *
+	 * @param endpoint
+	 */
+	public void example5(MqttEndpoint endpoint) {
 
-    // handling requests for unsubscriptions
-    endpoint.unsubscribeHandler(unsubscribe -> {
+		// handling requests for unsubscriptions
+		endpoint.unsubscribeHandler(unsubscribe -> {
 
-      for (String t: unsubscribe.topics()) {
-        System.out.println("Unsubscription for " + t);
-      }
-      // ack the subscriptions request
-      endpoint.unsubscribeAcknowledge(unsubscribe.messageId());
-    });
-  }
+			for (String t : unsubscribe.topics()) {
+				System.out.println("Unsubscription for " + t);
+			}
+			// ack the subscriptions request
+			endpoint.unsubscribeAcknowledge(unsubscribe.messageId());
+		});
+	}
 
-  /**
-   * Example for handling client published message
-   * @param endpoint
-   */
-  public void example6(MqttEndpoint endpoint) {
+	/**
+	 * Example for handling client published message
+	 *
+	 * @param endpoint
+	 */
+	public void example6(MqttEndpoint endpoint) {
 
-    // handling incoming published messages
-    endpoint.publishHandler(message -> {
+		// handling incoming published messages
+		endpoint.publishHandler(message -> {
 
-      System.out.println("Just received message [" + message.payload().toString(Charset.defaultCharset()) + "] with QoS [" + message.qosLevel() + "]");
+			System.out.println("Just received message [" + message.payload().toString(Charset.defaultCharset())
+					+ "] with QoS [" + message.qosLevel() + "]");
 
-      if (message.qosLevel() == MqttQoS.AT_LEAST_ONCE) {
-        endpoint.publishAcknowledge(message.messageId());
-      } else if (message.qosLevel() == MqttQoS.EXACTLY_ONCE) {
-        endpoint.publishRelease(message.messageId());
-      }
+			if (message.qosLevel() == MqttQoS.AT_LEAST_ONCE) {
+				endpoint.publishAcknowledge(message.messageId());
+			} else if (message.qosLevel() == MqttQoS.EXACTLY_ONCE) {
+				endpoint.publishRelease(message.messageId());
+			}
 
-    }).publishReleaseHandler(messageId -> {
+		}).publishReleaseHandler(messageId -> {
 
-      endpoint.publishComplete(messageId);
-    });
-  }
+			endpoint.publishComplete(messageId);
+		});
+	}
 
-  /**
-   * Example for handling publish message to the client
-   * @param endpoint
-   */
-  public void example7(MqttEndpoint endpoint) {
+	/**
+	 * Example for handling publish message to the client
+	 *
+	 * @param endpoint
+	 */
+	public void example7(MqttEndpoint endpoint) {
 
-    // just as example, publish a message with QoS level 2
-    endpoint.publish("my_topic",
-      Buffer.buffer("Hello from the Vert.x MQTT server"),
-      MqttQoS.EXACTLY_ONCE,
-      false,
-      false);
+		// just as example, publish a message with QoS level 2
+		endpoint.publish("my_topic", Buffer.buffer("Hello from the Vert.x MQTT server"), MqttQoS.EXACTLY_ONCE, false,
+				false);
 
-    // specifing handlers for handling QoS 1 and 2
-    endpoint.publishAcknowledgeHandler(messageId -> {
+		// specifing handlers for handling QoS 1 and 2
+		endpoint.publishAcknowledgeHandler(messageId -> {
 
-      System.out.println("Received ack for message = " +  messageId);
+			System.out.println("Received ack for message = " + messageId);
 
-    }).publishReceivedHandler(messageId -> {
+		}).publishReceivedHandler(messageId -> {
 
-      endpoint.publishRelease(messageId);
+			endpoint.publishRelease(messageId);
 
-    }).publishCompleteHandler(messageId -> {
+		}).publishCompleteHandler(messageId -> {
 
-      System.out.println("Received ack for message = " +  messageId);
-    });
-  }
+			System.out.println("Received ack for message = " + messageId);
+		});
+	}
 
-  /**
-   * Example for being notified by client keep alive
-   * @param endpoint
-   */
-  public void example8(MqttEndpoint endpoint) {
+	/**
+	 * Example for being notified by client keep alive
+	 *
+	 * @param endpoint
+	 */
+	public void example8(MqttEndpoint endpoint) {
 
-    // handling ping from client
-    endpoint.pingHandler(v -> {
+		// handling ping from client
+		endpoint.pingHandler(v -> {
 
-      System.out.println("Ping received from client");
-    });
-  }
+			System.out.println("Ping received from client");
+		});
+	}
 
-  /**
-   * Example for closing the server
-   * @param mqttServer
-   */
-  public void example9(MqttServer mqttServer) {
+	/**
+	 * Example for closing the server
+	 *
+	 * @param mqttServer
+	 */
+	public void example9(MqttServer mqttServer) {
 
-    mqttServer.close(v -> {
+		mqttServer.close(v -> {
 
-      System.out.println("MQTT server closed");
-    });
-  }
+			System.out.println("MQTT server closed");
+		});
+	}
 
-  /**
-   * Example for scaling (sharing MQTT servers)
-   * @param vertx
-   */
-  public void example10(Vertx vertx) {
+	/**
+	 * Example for scaling (sharing MQTT servers)
+	 *
+	 * @param vertx
+	 */
+	public void example10(Vertx vertx) {
 
-    for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 
-      MqttServer mqttServer = MqttServer.create(vertx);
-      mqttServer.endpointHandler(endpoint -> {
-        // handling endpoint
-      })
-        .listen(ar -> {
+			MqttServer mqttServer = MqttServer.create(vertx);
+			mqttServer.endpointHandler(endpoint -> {
+				// handling endpoint
+			}).listen(ar -> {
 
-          // handling start listening
-        });
+				// handling start listening
+			});
 
-    }
-  }
+		}
+	}
 
-  /**
-   * Example for scaling (sharing MQTT servers)
-   * @param vertx
-   */
-  public void example11(Vertx vertx) {
+	/**
+	 * Example for scaling (sharing MQTT servers)
+	 *
+	 * @param vertx
+	 */
+	public void example11(Vertx vertx) {
 
-    DeploymentOptions options = new DeploymentOptions().setInstances(10);
-    vertx.deployVerticle("com.mycompany.MyVerticle", options);
-  }
+		DeploymentOptions options = new DeploymentOptions().setInstances(10);
+		vertx.deployVerticle("com.mycompany.MyVerticle", options);
+	}
 }
